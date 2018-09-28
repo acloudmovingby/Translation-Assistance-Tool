69a30b6b07df6842a7f7ed3b5a0af220ca42c6c1
Chris Oates <chrispoates@gmail.com>
2018-09-28 19:07:39 -0400

    Thai Law Parser now deals with linebreaks and is now written more cleanly.

--- src/ParseThaiLaw/ParseThaiLaw.java
+++ src/ParseThaiLaw/ParseThaiLaw.java
@@ -18,11 +18,14 @@ public class ParseThaiLaw {
      */
     public static void main(String[] args) {
         
+        // BOOK 1
         String fileNameThai = "/Users/Chris/Desktop/Docs/Documents/Personal/Coding/Non-website design/Thai Parser Project/CAT1/src/CAT1/SampleThaiLaw1.txt";
         String fileNameEng = "/Users/Chris/Desktop/Docs/Documents/Personal/Coding/Non-website design/Thai Parser Project/CAT1/src/CAT1/SampleEnglishLaw1.txt";
-        
-        fileNameThai = "/Users/Chris/Desktop/Docs/Documents/Personal/Coding/Non-website design/Thai Parser Project/CAT1/src/CAT1/Thai Book 1TXT.txt";
         fileNameThai = "/Users/Chris/Desktop/Docs/Documents/Personal/Coding/Non-website design/Thai Parser Project/CAT1/src/CAT1/Thai Book 1TXT2.txt";
+        
+        // BOOK 2
+        fileNameThai = "/Users/Chris/Desktop/Docs/Documents/Personal/Coding/Non-website design/Thai Parser Project/CAT1/src/CAT1/Thai Book 2.txt";
+        fileNameEng = "/Users/Chris/Desktop/Docs/Documents/Personal/Coding/Non-website design/Thai Parser Project/CAT1/src/CAT1/Eng Book 2-1.txt";
 
         ThaiLawParser myParser = new ThaiLawParser(fileNameThai, fileNameEng);
         
